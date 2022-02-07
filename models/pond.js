561a61c63fef4ec6ec6fa5ec9a36355fffd90419
Jimmy1186 <sususu089@gmail.com>
2022-02-07 11:41:09 +0800

    read 16 bit and 32 bit use buffer

--- models/pond.js
+++ models/pond.js
@@ -3,27 +3,27 @@ const mongoose = require("mongoose");
 const pondSchema = new mongoose.Schema({
   DO: {
     type: Number,
-    set: (value) => Math.abs(Math.abs(value * 0.01)),
+    set: (value) => Math.abs(value*0.01).toFixed(2),
   },
   S: {
     type: Number,
-    set: (value) => Math.abs(value * 0.001).toFixed(2),
+    set: (value) => value.toFixed(2),
   },
   TEMP: {
     type: Number,
-    set: (value) => Math.abs(value * 0.001).toFixed(2),
+    set: (value) =>  value.toFixed(2),
   },
   ORP: {
     type: Number,
-    set: (value) => Math.abs(value * 0.01).toFixed(2),
+    set: (value) => value.toFixed(2),
   },
   PH: {
     type: Number,
-    set: (value) => Math.abs(value * 0.0001).toFixed(2),
+    set: (value) => value.toFixed(2),
   },
   WL: {
     type: Number,
-    set: (value) => Math.abs(value * 0.00005).toFixed(2),
+    set: (value) => value.toFixed(2),
   },
   IO: {
     type: Number,
--- server.js
+++ server.js
@@ -16,24 +16,15 @@ const {
 } = require("./models/pond");
 
 const options = {
-  host: "127.0.0.1",
-  port: 502,
+  host: "192.168.50.30",
+  port: "513",
 };
 const dbConnect = require("./utils/dbConnection");
 
-let DT100 = [],
-  DT200 = [],
-  DT300 = [],
-  POND_1,
-  POND_2,
-  POND_3,
-  POND_4,
-  POND_5,
-  POND_6,
-  POND_7,
-  POND_8,
-  POND_9,
-  POND_10;
+let DT100,
+  DT200,
+  DT300;
+
 const client = new Modbus.client.TCP(socket);
 
 
@@ -45,23 +36,16 @@ socket.on("connect", function () {
         .readHoldingRegisters(100 * i, 100)
         .then((resp) => {
           if (i === 1) {
-            DT100 = resp.response._body.valuesAsArray;
-            POND_1 = DT100.slice(0, 14);
+            DT100 = resp.response._body.valuesAsBuffer;
+          
           }
           if (i === 2) {
-            DT200 = resp.response._body.valuesAsArray;
-            POND_2 = DT200.slice(0, 14);
-            POND_3 = DT200.slice(20, 34);
-            POND_4 = DT200.slice(40, 54);
-            POND_5 = DT200.slice(60, 74);
-            POND_6 = DT200.slice(80, 94);
+            DT200 = resp.response._body.valuesAsBuffer;
+           
           }
           if (i === 3) {
-            DT300 = resp.response._body.valuesAsArray;
-            POND_7 = DT300.slice(0, 14);
-            POND_8 = DT300.slice(20, 34);
-            POND_9 = DT300.slice(40, 54);
-            POND_10 = DT300.slice(60, 74);
+            DT300 = resp.response._body.valuesAsBuffer;
+        
           }
         })
         .catch((err) => {
@@ -72,95 +56,95 @@ socket.on("connect", function () {
     await dbConnect();
 
     POND_1_COLLECTION.create({
-      DO: POND_1[0],
-      S: POND_1[2],
-      TEMP: POND_1[4],
-      ORP: POND_1[6],
-      PH: POND_1[8],
-      WL: POND_1[10],
-      IO: POND_1[12],
+      DO: DT100.readInt16BE(0),
+      S: DT100.readFloatBE(6),
+      TEMP: DT100.readFloatBE(10),
+      ORP: DT100.readFloatBE(14),
+      PH: DT100.readFloatBE(18),
+      WL: DT100.readFloatBE(22),
+      IO: DT100.readInt16BE(24),
     });
 
     POND_2_COLLECTION.create({
-      DO: POND_2[0],
-      S: POND_2[2],
-      TEMP: POND_2[4],
-      ORP: POND_2[6],
-      PH: POND_2[8],
-      WL: POND_2[10],
-      IO: POND_2[12],
+      DO: DT200.readInt16BE(0),
+      S: DT200.readFloatBE(6),
+      TEMP: DT200.readFloatBE(10),
+      ORP: DT200.readFloatBE(14),
+      PH: DT200.readFloatBE(18),
+      WL: DT200.readFloatBE(22),
+      IO: DT200.readInt16BE(24),
     });
     POND_3_COLLECTION.create({
-      DO: POND_3[0],
-      S: POND_3[2],
-      TEMP: POND_3[4],
-      ORP: POND_3[6],
-      PH: POND_3[8],
-      WL: POND_3[10],
-      IO: POND_3[12],
+      DO: DT200.readInt16BE(40),
+      S: DT200.readFloatBE(46),
+      TEMP: DT200.readFloatBE(50),
+      ORP: DT200.readFloatBE(54),
+      PH: DT200.readFloatBE(58),
+      WL: DT200.readFloatBE(62),
+      IO: DT200.readInt16BE(64),
     });
     POND_4_COLLECTION.create({
-      DO: POND_4[0],
-      S: POND_4[2],
-      TEMP: POND_4[4],
-      ORP: POND_4[6],
-      PH: POND_4[8],
-      WL: POND_4[10],
-      IO: POND_4[12],
+      DO: DT200.readInt16BE(80),
+      S: DT200.readFloatBE(86),
+      TEMP: DT200.readFloatBE(90),
+      ORP: DT200.readFloatBE(94),
+      PH: DT200.readFloatBE(98),
+      WL: DT200.readFloatBE(102),
+      IO: DT200.readInt16BE(104),
     });
     POND_5_COLLECTION.create({
-      DO: POND_5[0],
-      S: POND_5[2],
-      TEMP: POND_5[4],
-      ORP: POND_5[6],
-      PH: POND_5[8],
-      WL: POND_5[10],
-      IO: POND_5[12],
+      DO: DT200.readInt16BE(120),
+      S: DT200.readFloatBE(126),
+      TEMP: DT200.readFloatBE(130),
+      ORP: DT200.readFloatBE(134),
+      PH: DT200.readFloatBE(138),
+      WL: DT200.readFloatBE(142),
+      IO: DT200.readInt16BE(144),
     });
     POND_6_COLLECTION.create({
-      DO: POND_6[0],
-      S: POND_6[2],
-      TEMP: POND_6[4],
-      ORP: POND_6[6],
-      PH: POND_6[8],
-      WL: POND_6[10],
-      IO: POND_6[12],
+      DO: DT200.readInt16BE(160),
+      S: DT200.readFloatBE(166),
+      TEMP: DT200.readFloatBE(170),
+      ORP: DT200.readFloatBE(174),
+      PH: DT200.readFloatBE(178),
+      WL: DT200.readFloatBE(182),
+      IO: DT200.readInt16BE(184),
     });
     POND_7_COLLECTION.create({
-      DO: POND_7[0],
-      S: POND_7[2],
-      TEMP: POND_7[4],
-      ORP: POND_7[6],
-      PH: POND_7[8],
-      WL: POND_7[10],
-      IO: POND_7[12],
+      DO: DT300.readInt16BE(0),
+      S: DT300.readFloatBE(6),
+      TEMP: DT300.readFloatBE(10),
+      ORP: DT300.readFloatBE(14),
+      PH: DT300.readFloatBE(18),
+      WL: DT300.readFloatBE(22),
+      IO: DT300.readInt16BE(24),
     });
     POND_8_COLLECTION.create({
-      DO: POND_8[0],
-      S: POND_8[2],
-      TEMP: POND_8[4],
-      ORP: POND_8[6],
-      PH: POND_8[8],
-      WL: POND_8[10],
-      IO: POND_8[12],
+      DO: DT300.readInt16BE(40),
+      S: DT300.readFloatBE(46),
+      TEMP: DT300.readFloatBE(50),
+      ORP: DT300.readFloatBE(54),
+      PH: DT300.readFloatBE(58),
+      WL: DT300.readFloatBE(62),
+      IO: DT300.readInt16BE(64),
     });
     POND_9_COLLECTION.create({
-      DO: POND_9[0],
-      S: POND_9[2],
-      TEMP: POND_9[4],
-      ORP: POND_9[6],
-      PH: POND_9[8],
-      WL: POND_9[10],
-      IO: POND_9[12],
+      DO: DT300.readInt16BE(80),
+      S: DT300.readFloatBE(86),
+      TEMP: DT300.readFloatBE(90),
+      ORP: DT300.readFloatBE(94),
+      PH: DT300.readFloatBE(98),
+      WL: DT300.readFloatBE(102),
+      IO: DT300.readInt16BE(104),
     });
     POND_10_COLLECTION.create({
-      DO: POND_10[0],
-      S: POND_10[2],
-      TEMP: POND_10[4],
-      ORP: POND_10[6],
-      PH: POND_10[8],
-      WL: POND_10[10],
-      IO: POND_10[12],
+      DO: DT300.readInt16BE(120),
+      S: DT300.readFloatBE(126),
+      TEMP: DT300.readFloatBE(130),
+      ORP: DT300.readFloatBE(134),
+      PH: DT300.readFloatBE(138),
+      WL: DT300.readFloatBE(142),
+      IO: DT300.readInt16BE(144),
     });
     console.log(`Data success inert at ${Date(Date.now())}`)
   }
